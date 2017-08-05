fc5b33eb0cc2fd530c3bdd3a22b8003fe41bf538
levimiller <levi@levimiller.ca>
2017-08-05 13:31:45 -0700

    moved compilation of regex in CapitalizeRowsTranslator to a static final attribute to remove repeated compilations.

--- src/main/java/se/pingstteknik/propresenter/stagedisplayviewer/util/CapitalizeRowsTranslator.java
+++ src/main/java/se/pingstteknik/propresenter/stagedisplayviewer/util/CapitalizeRowsTranslator.java
@@ -4,17 +4,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CapitalizeRowsTranslator {
-	/**
-	 * Capitalizes the first word of every line.
-	 * @param text
-	 * @return
-	 */
-	public static String transform(String text) {
-		Matcher m = Pattern.compile("(?<=\n|^)([a-z])").matcher(text);
-		StringBuffer result = new StringBuffer();
-		while(m.find()) // for each match, replace with capitalized text.
-			m.appendReplacement(result, m.group().toUpperCase());
-		m.appendTail(result);
-		return result.toString();
-	}
+    private static final Pattern FIRST_CHAR_REGEX = Pattern.compile("(?<=\n|^)([a-z])");
+    
+    /**
+     * Capitalizes the first word of every line.
+     * @param text
+     * @return
+     */
+    public static String transform(String text) {
+	Matcher m = FIRST_CHAR_REGEX.matcher(text);
+	StringBuffer result = new StringBuffer();
+	while(m.find()) // for each match, replace with capitalized text.
+	    m.appendReplacement(result, m.group().toUpperCase());
+	m.appendTail(result);
+	return result.toString();
+    }
 }
